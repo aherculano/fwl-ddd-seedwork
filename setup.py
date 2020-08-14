92b617eb6e66c69e3c2cbf9c6850963387c5de27
Alexandre Rodrigues (1170944) <1170944@isep.ipp.pt>
2020-08-14 15:40:21 +0100

    cenas

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='fwl-ddd-seedwork',  # How you named your package folder (MyLib)
-    version='0.1',
+    version='0.2',
     license='MIT',
     description='simple ddd seedwork',
     author='Alexandre Herculano',
